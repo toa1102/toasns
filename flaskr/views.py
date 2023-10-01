512918390f3d279802135404650786b1fceec61b
toa1102 <toatoauehara@gmail.com>
2023-10-01 13:24:53 +0900

    new

--- flaskr/views.py
+++ flaskr/views.py
@@ -3,3 +3,22 @@ from flaskr.forms import LikeAnimeForm
 
 app = Flask(__name__)
 
+#@app.routeの引数にmethods=['GET', 'POST']を追加
+#これはユーザからフォームを介して変数を取得するので、
+#HTTPメソッドとしてPOSTを許可するという意味。
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    #フォームはform = LikeAnimeForm(request.form)という形で記述
+    #そのため、forms.pyからLikeAnimeFormとflaskライブラリからrequestをimportする必要がある。
+    #requestというのは、ユーザから送信された変数を取得するメソッドであり、
+    #フォームからの場合はrequest.formというプロパティを取得することになる。
+    form = LikeAnimeForm(request.form)
+    name = like_anime = rate = None
+    #ここから下でそれぞれフォームから取得した値を変数として宣言している。
+    #ここで作成した変数をrender_templateの引数でそれぞれ再びindex.htmlに返している。
+    if request.method == 'POST':
+        name = form.name.data
+        like_anime = form.like_anime.data
+        rate = form.name.data
+        return render_template('index.html', form=form, name=name, like_anime=like_anime, rate=rate)
+    return render_template('index.html', form=form, name=name, like_anime=like_anime, rate=rate)
