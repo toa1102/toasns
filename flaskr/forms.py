82b2503b92448ad80737543f9bfb1c038421cbde
toa1102 <toatoauehara@gmail.com>
2023-10-01 13:00:54 +0900

    new

---
+++ flaskr/forms.py
@@ -0,0 +1,8 @@
+from wtforms.form import Form
+from wtforms.fields import IntegerField, StringField, SubmitField
+
+class LikeAnimeForm(Form):
+    name = StringField('名前：')
+    like_anime = StringField('好きなアニメは：')
+    rate = IntegerField('点数をつけるなら：')
+
---
+++ flaskr/views.py
@@ -0,0 +1,5 @@
+from flask import Flask, render_template, request
+from flaskr.forms import LikeAnimeForm
+
+app = Flask(__name__)
+
