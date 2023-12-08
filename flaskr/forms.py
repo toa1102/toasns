6794551de4cd4bb3b36d1d58b9420ece2369330c
toa1102 <toatoauehara@gmail.com>
2023-12-08 20:03:06 +0900

    new

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,8 +1,31 @@
 from wtforms.form import Form
-from wtforms.fields import IntegerField, StringField, SubmitField
+from wtforms.fields import (
+    IntegerField, StringField, TextField, TextAreaField, PasswordField,
+    HiddenField, SubmitField, FileField
+)
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from flaskr.views import User
 
-class LikeAnimeForm(Form):
-    name = StringField('名前：')
-    like_anime = StringField('好きなアニメは：')
-    rate = IntegerField('点数をつけるなら：')
-    submit = SubmitField('送信')
+
+class LoginForm(Form):
+    email = StringField('メールアドレス', validators=[DataRequired(), Email()])
+    password = PasswordField('パスワード', validators=[DataRequired()])
+    conf_password = PasswordField('確認用パスワード', validators=[DataRequired(), EqualTo('password', message='元のパスワードと一致しません')])
+    submit = SubmitField('ログイン')
+
+    def validate_password(self, field):
+        if len(field.data) < 4:
+            raise ValidationError('パスワードは4文字以上で！')
+
+
+class RegisterForm(Form):
+    username = StringField('ユーザ名', validators=[DataRequired()])
+    email = StringField('メールアドレス', validators=[DataRequired(), Email()])
+    password = PasswordField('パスワード', validators=[DataRequired()])
+    conf_password = PasswordField('確認用パスワード', validators=[DataRequired(), EqualTo('password', message='元のパスワードと一致しません')])
+    submit = SubmitField('ユーザ登録')
+
+    def validate_email(self, field):
+        if User.select_by_email(field.data):
+            raise ValidationError('すでに登録されているメールアドレスです')
